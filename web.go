90e8eee50409ec1139ea93eb9b71083a189ac3ce
JonErikDSuero <jonerikdsuero@Jons-MacBook-Air.local>
2014-07-29 20:14:24 -0400

    try connecting to mongodb

--- web.go
+++ web.go
@@ -42,8 +42,12 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 func CreateLinkHandler(w http.ResponseWriter, r *http.Request) {
   r.ParseForm()
   fmt.Printf("name: !%s!\n", r.FormValue("name"))
-  //fmt.Printf("Vars: !%s!\n", vars)
-  session, err := mgo.Dial("localhost:27017")
+
+  var mongo_url= os.Getenv("MONGO_URL")
+  if (mongo_url == "") {
+    mongo_url = "localhost:27017"
+  }
+  session, err := mgo.Dial(mongo_url)
   if err != nil {
     panic(err)
   }
@@ -52,9 +56,16 @@ func CreateLinkHandler(w http.ResponseWriter, r *http.Request) {
   session.SetMode(mgo.Monotonic, true)
   con := session.DB("test").C("link")
   err = con.Insert( &Link{ r.FormValue("name"), r.FormValue("url"), r.FormValue("submitted_by") } )
-  //fmt.Printf("Name: !%s!\n", vars["name"])
   if err != nil {
     panic(err)
   }
   fmt.Fprintf(w, "SemiWorking...")
 }
+
+// MONGODB_DATABASE: "linkrary_go-production"
+// MONGODB_HOST:     "172.17.0.129"
+// MONGODB_PORT:     "27017"
+// MONGODB_USERNAME: "linkrary_go"
+// MONGODB_PASSWORD: "QkhxVE5JSzd3SFRMWW9ZTmV1TElocWNjbHlLVGd0SXlhQ0dEQ3ZJMm8zaz0K"
+// MONGO_URL:        "mongodb://linkrary_go:QkhxVE5JSzd3SFRMWW9ZTmV1TElocWNjbHlLVGd0SXlhQ0dEQ3ZJMm8zaz0K@172.17.0.129:27017/linkrary_go-production"
+
