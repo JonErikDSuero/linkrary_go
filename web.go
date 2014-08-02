b7fa6b1697b4d7aea6e8d7ea0ef0be93c679b50f
JonErikDSuero <jonerikdsuero@Jons-MacBook-Air.local>
2014-08-02 06:57:47 -0400

    can create links

--- web.go
+++ web.go
@@ -7,70 +7,114 @@ import (
   "net/http"
   "github.com/gorilla/mux"
   "gopkg.in/mgo.v2"
-  "strings"
-  //"gopkg.in/mgo.v2/bson"
+  //"strings"
+  "gopkg.in/mgo.v2/bson"
+  "log"
+)
+
+type (
+  mgoRepo struct {
+    Collection *mgo.Collection
+  }
+
+  Links []Link
+  Link struct {
+    Id           bson.ObjectId  `json:"id"            bson:"_id"`
+    Name         string         `json:"name"          bson:"name"`
+    Url          string         `json:"url"           bson:"url"`
+    SubmittedBy  string         `json:"submitted_by"  bson:"submitted_by"`
+  }
 )
 
 
-type Link struct {
-  Name string
-  Url string
-  SubmittedBy string
-}
+var linkRepo mgoRepo
 
 
 func main() {
+  var mongo_url = os.Getenv("MY_MONGODB_URL")
+  if (mongo_url == "") {
+    mongo_url = "localhost:27017"
+  }
+  session, err := mgo.Dial(mongo_url)
+  if err != nil { panic(err) }
+  defer session.Close()
+  session.SetMode(mgo.Monotonic, true)  // Optional. Switch the session to a monotonic behavior.
+  linkRepo.Collection = session.DB("linkrary_go-production").C("link")
+
   r := mux.NewRouter()
-  r.HandleFunc("/", HomeHandler).Methods("GET")
-  r.HandleFunc("/links/create", CreateLinkHandler).Methods("POST")
+  r.HandleFunc("/", handleHome).Methods("GET")
+  r.HandleFunc("/links/create", handleLinkCreate).Methods("POST")
   http.Handle("/", r)
   var port = os.Getenv("PORT")
   if (port == "") {
     port = "3000"
   }
-  err := http.ListenAndServe(":"+port, nil)
-  if err != nil {
-    panic(err)
-  }
+  err = http.ListenAndServe(":"+port, nil)
+  if err != nil { panic(err) }
 }
 
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
-  for _, e := range os.Environ() {
-    pair := strings.Split(e, "=")
-    fmt.Println( pair[0] + "=" + pair[1] )
-  }
-  var mongo_url = os.Getenv("MONGO_URL")
-  if (mongo_url == "") {
-    mongo_url = "localhost:27017"
-  }
-  fmt.Printf("mongo_url is [%s]\n", mongo_url)
-  session, err := mgo.Dial(mongo_url)
-  if err != nil {
-    panic(err)
-  }
-  defer session.Close()
+func handleHome(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Hello!")
 }
 
 
-func CreateLinkHandler(w http.ResponseWriter, r *http.Request) {
+func handleLinkCreate(w http.ResponseWriter, r *http.Request) {
+  var (
+    link Link
+    err  error
+  )
+  data := struct {
+    Success bool `json:"success"`
+    Link    Link `json:"link"`
+  }{
+    Success: false,
+  }
   r.ParseForm()
-  var mongo_url = r.FormValue("mongo_url")
-  if (mongo_url == "") {
-    mongo_url = "localhost:27017"
-  }
-  fmt.Printf("mongo_url !%s!\n", mongo_url)
-  session, err := mgo.Dial(mongo_url)
-  if err != nil {
+  link.Name = r.FormValue("name")
+  link.Url = r.FormValue("url")
+  link.SubmittedBy = r.FormValue("submitted_by")
+  if err = linkRepo.Create(&link); err != nil {
     panic(err)
+  } else {
+    data.Success = true
+    data.Link = link
   }
-  defer session.Close()
-  // Optional. Switch the session to a monotonic behavior.
-  session.SetMode(mgo.Monotonic, true)
-  con := session.DB("linkrary_go-production").C("link")
-  err = con.Insert( &Link{ r.FormValue("name"), r.FormValue("url"), r.FormValue("submitted_by") } )
-  if err != nil {
-    panic(err)
-  }
-  fmt.Fprintf(w, "SemiWorking...")
+
+  writeJson(w, data)
 }
+
+
+func (r mgoRepo) Create(link *Link) (err error) {
+  if link.Id.Hex() == "" {
+    link.Id = bson.NewObjectId()
+  }
+  //if link.Created.IsZero() {
+  //  link.Created = time.Now()
+  //}
+  //link.Updated = time.Now()
+  _, err = r.Collection.UpsertId(link.Id, link)
+  return
+}
+
+
+func handleLinks(w http.ResponseWriter, r *http.Request) {
+  var (
+    links Links
+    err   error
+  )
+  if links, err = linkRepo.All(); err != nil {
+    log.Printf("%v", err)
+    http.Error(w, "500 Internal Server Error", 500)
+    return
+  }
+  writeJson(w, links)
+}
+
+func (r mgoRepo) All() (links Links, err error) {
+  err = r.Collection.Find(bson.M{}).All(&links)
+  return
+}
+
+
+
